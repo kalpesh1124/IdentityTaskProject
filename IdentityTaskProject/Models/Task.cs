9c7a553e4d11291e0365fb1a5457fda052ac225d
kalpesh1124 <kalpeshinde170@gmail.com>
2024-02-09 20:57:01 +0530

    Add project files.

---
+++ IdentityTaskProject/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IdentityTaskProject.Models;
+
+public partial class Task
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Description { get; set; }
+
+    public DateTime? Date { get; set; }
+
+    public int? Userid { get; set; }
+}
